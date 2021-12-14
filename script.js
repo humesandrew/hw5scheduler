1b4ad54f8113c89f38e0629002dc4d761cf0cb8e
Andy Humes <andrew.humes@du.edu>
2021-12-14 11:55:54 -0700

    added more hours, commented

--- script.js
+++ script.js
@@ -1,93 +1,61 @@
-
-
-// const m = moment();
-// const displayCurrentDate = m.format("[Today is] dddd, MMM, YYYY");
-
-
-// $("#currentDay").text(displayCurrentDate);
-
-// want to grab what they write now for 8am//
-
-// const eight = $('.primary');
-
-
-//working on current hour color//
-
-
-// const currentTime = $("#12");
-// const currentHour = m.get("hour");
-
-
-
-// function setCurrentTime() {
-//   if (currentTime == currentHour) {
-//     console.log("great");
-//     currentTime.css("background-color", "green");
-//   }
-// };
-
-// function convertID() {
- 
-//   console.log(currentTime);
-
-
-
-
-
-  // if (currentTime) {
-  //   currentTime.css("background-color", "green");
-  // }
-  // else {
-
-  // }
-   
-   
-  // };//
-
-// setCurrentTime();
-// convertID();
-
-
-
-
-// m.get("hour");
-
-// console.log(m.format("HH"));
-
-
-
+// added function to button click that sets the local storage with that value//
 
 $(document).ready(function() {
-
   $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".textItem").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
   });
 
-function updateHour() {
-  var currentHour = moment().hours();
-  $(".blockTime").each(function() {
-  var hourBlock = parseInt($(this).attr("id").split("-")[1]);
-if (hourBlock > moment().hours()) {
-$(".blockTime").css("background-color: red");
-}
-  })
-}
 
-updateHour();
-var interval = setInterval(updateHour, 15000);
-
+  // made it so the local storage data is persistent//
 $("#hour-8 .textItem").val(localStorage.getItem("hour-8"));
 $("#hour-9 .textItem").val(localStorage.getItem("hour-9"));
 $("#hour-10 .textItem").val(localStorage.getItem("hour-10"));
+$("#hour-11 .textItem").val(localStorage.getItem("hour-11"));
+$("#hour-12 .textItem").val(localStorage.getItem("hour-12"));
+$("#hour-13 .textItem").val(localStorage.getItem("hour-13"));
+$("#hour-14 .textItem").val(localStorage.getItem("hour-14"));
+$("#hour-15 .textItem").val(localStorage.getItem("hour-15"));
+$("#hour-16 .textItem").val(localStorage.getItem("hour-16"));
 
 
-$("#currentDay").text(moment().format("Do MMM YYYY"));
-
+// used moment to indicate the current day for the user//
+$("#currentDay").text("Today is: " + moment().format("Do MMM YYYY"));
 });
 
 
+console.log($(".hour").val());
+
+
+
+
+
+
+//working on converting each slot to a number to compare to moment//
+// $(".textItem").css("background-color", "red");
+
+// function renderColor() {
+//   if ( ".hour" == NaN) {
+//     $(".textItem").css("background-color", "green")
+
+//   }
+//   }
+
+
+
+// function updateHour() {
+//   var currentHour = moment().hours();
+//   $(".blockTime").each(function() {
+//   var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+// if (hourBlock > moment().hours()) {
+// $(".blockTime").css("background-color: red");
+// }
+//   })
+// }
+
+// updateHour();
+// var interval = setInterval(updateHour, 15000);
 
 
 
@@ -100,29 +68,4 @@ $("#currentDay").text(moment().format("Do MMM YYYY"));
 
 
 
-// function colorPast() {
-// //   if (today) {
-// // console.log("hello");//
-// // eight.css('background-color', 'red');
-// //   }
-  
 
-
-
-// function colorCurrent() {
-//   // today.format("w");//
-//   console.log(m);
-//   // if (eight < today)
-//   // {eight.css('background-color', 'green')};//
-
-
-
-// // function colorFuture() {
-// //   if (eight) {
-// //     console.log("great");
-// //   }
-// // }//
-
-// // colorPast();//
-
-// colorCurrent();
